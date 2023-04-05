1fd971a7377658e24749e1329dd78463166a31f6
Marcojal <marcojal@hotmail.com>
2023-04-05 10:49:04 +0000

    .

--- models/Persona.php
+++ models/Persona.php
@@ -1,27 +1,59 @@
 <?php
-declare(strict_types = 1);
 
-class Persona {
+declare(strict_types=1);
+
+class Persona
+{
+ protected string $dni = '';
  public string $nombre = 'Desconocido';
- public string $pais = 'Desconocido';
- private int $edad = 50;
+ public int $edad = 50;
 
  // Método constructor en php:
- function __construct (?string $name, ?int $edad) {
-  $this->nombre = $name??'';
-  $this->edad = $edad??0;
+ function __construct(string $nif, ?string $name, ?int $age)
+ {
+  $this->dni = $nif ?? 0;
+  $this->nombre = $name ?? 'Desconocido';
+  $this->edad = $age ?? 0;
  }
 
- // Pasar a string
- function __toString () {
-  return join (
-   ',',
-   [$this->nombre, $this->pais, $this->edad]
-  );
+ function setNif(string $nif): void
+ {
+  $this->dni = $nif;
  }
 
  // Dar acceso al contenido privado:
- public function getEdad () : int {
+ public function getEdad(): int
+ {
   return $this->edad;
  }
+ function getNif(): string
+ {
+  return $this->dni;
+ }
+
+
+ // Pasar a string
+ function __toString()
+ {
+  return join(
+   ',',
+   [$this->nombre, $this->dni, $this->edad]
+  );
+ }
 }
+
+class Profesor extends Persona
+{
+ public array $asignaturas = [];
+ public bool $esTutor = false;
+ function __construct(string $nif, string $name, int $age, bool $tutor)
+ {
+  parent::__construct($nif, $name, $age);
+  $this->esTutor = $tutor;
+ }
+}
+
+$xurxo = new Profesor('32443r32', 'pepe', 54, true);
+array_push($xurxo->asignaturas, ['Desarrollo Web', 'BBDD']);
+
+print_r($xurxo);
